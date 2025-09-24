1963ea42e1bfcf38a52ab7b9ee1bd2223eb99e82
hidayattresnadi <hidayattresnadi64@gmail.com>
2025-09-24 16:08:42 +0700

    Feature : Add Profile Screen

---
+++ lib/screens/profile/profile_screen.dart
@@ -0,0 +1,250 @@
+import 'package:community_report_app/models/profile.dart';
+import 'package:community_report_app/provider/profileProvider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final profile = context.watch<ProfileProvider>().profile;
+
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        body: Column(
+          children: [
+            buildProfileSection(profile),
+            const SizedBox(height: 130),
+            const TabBar(
+              labelColor: Colors.black,
+              unselectedLabelColor: Colors.grey,
+              indicatorColor: Colors.green,
+              tabs: [
+                Tab(text: "Posts"),
+                Tab(text: "Likes"),
+              ],
+            ),
+
+            // Konten tab (flexible, isi layar)
+            Expanded(
+              child: TabBarView(
+                children: [
+                  // contoh dummy posts
+                  ListView.builder(
+                    padding: const EdgeInsets.only(bottom: 16),
+                    itemCount:
+                        10, // jumlah dummy post (nanti bisa pakai data asli)
+                    itemBuilder: (context, index) {
+                      return buildPostSection(profile, context);
+                    },
+                  ),
+                  // contoh dummy likes
+                  ListView.builder(
+                    itemCount: 5,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        leading: const Icon(Icons.favorite, color: Colors.red),
+                        title: Text("Liked Post #$index"),
+                        subtitle: const Text("This is a liked post"),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+Widget buildProfileSection(Profile? profile) {
+  return Stack(
+    clipBehavior: Clip.none,
+    children: [
+      buildCoverProfile(),
+      Positioned(
+        top: 130,
+        left: 30,
+        right: 30,
+        child: buildProfileHeader(profile),
+      ),
+    ],
+  );
+}
+
+Widget buildCoverProfile() {
+  return Container(height: 195, color: const Color(0xFFD9D9D9));
+}
+
+Widget buildProfileHeader(Profile? profile) {
+  return Stack(
+    children: [
+      Column(
+        children: [
+          CircleAvatar(
+            radius: 50,
+            backgroundImage:
+                (profile?.photo != null && profile!.photo!.isNotEmpty)
+                ? NetworkImage(profile.photo!)
+                : null,
+            child: (profile?.photo == null || profile!.photo!.isEmpty)
+                ? const Icon(Icons.person, size: 50)
+                : null,
+          ),
+          const SizedBox(height: 10),
+          Text(
+            '${profile?.front_name ?? "-"} ${profile?.last_name ?? "-"}',
+            style: const TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w600,
+              fontFamily: 'Roboto',
+              color: Colors.black,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          Text(
+            profile?.username ?? "Not specified",
+            style: const TextStyle(fontSize: 14, color: Colors.black54),
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+      Positioned(right: 0, top: 90, child: buildEditProfileButton()),
+    ],
+  );
+}
+
+Widget buildEditProfileButton() {
+  return Container(
+    padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+    decoration: ShapeDecoration(
+      color: const Color(0xFF249A00),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(7)),
+    ),
+    child: const Text(
+      'Edit Profile',
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 15,
+        fontFamily: 'Roboto',
+        fontWeight: FontWeight.w400,
+      ),
+    ),
+  );
+}
+
+Widget buildPostSection(Profile? profile, BuildContext context) {
+  final screenWidth = MediaQuery.of(context).size.width;
+  final horizontalPadding = screenWidth * 0.07;
+
+  return Column(
+    children: [
+      Padding(
+        padding: EdgeInsets.fromLTRB(
+          horizontalPadding,
+          30,
+          horizontalPadding,
+          0,
+        ),
+        child: Row(
+          children: [
+            CircleAvatar(
+              radius: 20,
+              backgroundImage:
+                  (profile?.photo != null && profile!.photo!.isNotEmpty)
+                  ? NetworkImage(profile.photo!)
+                  : null,
+              child: (profile?.photo == null || profile!.photo!.isEmpty)
+                  ? const Icon(Icons.person, size: 20)
+                  : null,
+            ),
+            const SizedBox(width: 10),
+
+            Text(
+              '${profile?.front_name ?? "-"} ${profile?.last_name ?? "-"}',
+              style: const TextStyle(
+                color: Colors.black,
+                fontSize: 16,
+                fontFamily: 'Roboto',
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            const SizedBox(width: 10),
+
+            Text(
+              '22 hours ago',
+              style: const TextStyle(
+                color: Color(0xFF249A00),
+                fontSize: 13,
+                fontFamily: 'Roboto',
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+
+            const Spacer(),
+
+            Container(
+              width: 57,
+              height: 25,
+              decoration: BoxDecoration(
+                color: const Color(0xFFE0FFDE),
+                borderRadius: BorderRadius.circular(5),
+              ),
+              alignment: Alignment.center,
+              child: const Text(
+                'Safety',
+                style: TextStyle(
+                  color: Color(0xFF249A00),
+                  fontSize: 13,
+                  fontFamily: 'Roboto',
+                  fontWeight: FontWeight.normal,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      Padding(
+        padding: EdgeInsetsGeometry.only(top: 7, left: 75, right: 25),
+        child: Column(
+          children: [
+            SizedBox(
+              width: 328,
+              child: Text(
+                'The pedestrian signal at the main intersection near Mexico Square is not functioning. Cars don’t stop, and people are forced to cross dangerously. This puts children and elderly at high risk. Please fix urgently.',
+                style: TextStyle(
+                  color: Colors.black /* Black */,
+                  fontSize: 13,
+                  fontFamily: 'Roboto',
+                  fontWeight: FontWeight.w400,
+                  height: 1.92,
+                ),
+              ),
+            ),
+            const SizedBox(height: 7),
+            Container(
+              width: double.infinity,
+              height: 145,
+              decoration: ShapeDecoration(
+                image: DecorationImage(
+                  image: NetworkImage(
+                    "https://dishub.banjarmasinkota.go.id/wp-content/uploads/2024/11/lampu-lalu-lintas-punya-3-warna_169.jpg",
+                  ),
+                  fit: BoxFit.cover,
+                ),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ],
+  );
+}
